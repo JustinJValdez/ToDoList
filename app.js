9286c6f47acdb5178cb616b031cdc44c32c5c5ee
Justin Valdez <justinjvaldez@gmail.com>
2020-12-13 22:05:00 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+const app = express();
+
+
+let items = ["prep", "cook", "eat"];
+let workItems =[];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+    
+let today = new Date();
+
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {listTitle: day, newListItems: items}); 
+// need to put multiple EJS items in same render to keep code stable.
+});
+
+app.post("/", (req,res)=>{
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", (req,res)=>{
+res.render("list", {listTitle:"Work List", newListItems:workItems});
+
+});
+
+app.get("/about", (req,res)=>{
+    res.render("about");
+})
+
+
+
+app.listen(3000, function(){
+    console.log("Server has started on port 3000");
+})
